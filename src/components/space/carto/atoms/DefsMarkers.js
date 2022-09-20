c54a99af92047b0d6c864b1742670cda0b92a1e5
Tushar Agarwal <niftytushar@gmail.com>
2022-09-20 21:38:00 +0530

    Update marker style based on selected event

--- src/components/space/carto/atoms/DefsMarkers.js
+++ src/components/space/carto/atoms/DefsMarkers.js
@@ -1,43 +1,63 @@
+import dayjs from "dayjs";
 import React from "react";
+import { connect } from "react-redux";
 
-var date = new Date();
-var day = date.getDate();
-day = day < 10 ? "0" + day : day;
-var month = date.getMonth() + 1;
-month = month < 10 ? "0" + month : month;
-var year = date.getFullYear();
-var today = day + "/" + month + "/" + year;
-
-const MapDefsMarkers = ({ markers, projectPoint, narrative }) => {
+const MapDefsMarkers = ({ markers, projectPoint, narrative, app }) => {
   if (markers === undefined) return null;
+
+  const { selected } = app;
+
+  let seletedEventDate;
+
+  if (selected?.[0]?.date && selected?.[0]?.time) {
+    seletedEventDate = dayjs(`${selected[0].date} ${selected[0].time}`);
+  }
+
   return (
     <>
       {markers.map((marker) => {
         const { x, y } = projectPoint([marker.latitude, marker.longitude]);
-        if (marker.startdate <= today) {
-          return (
-            <svg>
-              <g
-                className={`location-event ${narrative ? "no-hover" : ""}`}
-                transform={`translate(${x}, ${y})`}
-              >
-                {/* <circle cx="0" cy="0" r="10" stroke="black" stroke-width="2"  */}
-                {/* fill= {marker.enddate>=today?"yellow":"none"} stroke-opacity="0.8" fill-opacity="0.8"/> */}
-                <rect
-                  width="20"
-                  height="20"
-                  stroke="blue"
-                  stroke-width="2"
-                  fill={marker.enddate >= today ? "yellow" : "none"}
-                  fill-opacity="0.8"
-                />
-              </g>
-            </svg>
-          );
-        } else return null;
+
+        const styles = {};
+        if (seletedEventDate.isAfter(dayjs(marker.enddate, "MM/DD/YYYY"))) {
+          styles.stroke = "yellow";
+          styles["stroke-width"] = 2;
+          styles["stroke-dasharray"] = "2,2";
+          styles["strokeLinejoin"] = "round";
+        } else {
+          styles.fill = "yellow";
+          styles["fill-opacity"] = "0.8";
+        }
+
+        return (
+          <svg>
+            <g
+              className={`location-event ${narrative ? "no-hover" : ""}`}
+              transform={`translate(${x}, ${y})`}
+            >
+              {/* <circle cx="0" cy="0" r="10" stroke="black" stroke-width="2"  */}
+              {/* fill= {marker.enddate>=today?"yellow":"none"} stroke-opacity="0.8" fill-opacity="0.8"/> */}
+              <rect width="14" height="14" {...styles} />
+            </g>
+          </svg>
+        );
       })}
     </>
   );
 };
 
-export default MapDefsMarkers;
+function mapStateToProps(state) {
+  return {
+    app: {
+      selected: state.app.selected,
+    },
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: {},
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MapDefsMarkers);
